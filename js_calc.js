f5fcfaaeabd9703ab9d9093c111becb3bb8d67dc
Олег <73531304+olegus413@users.noreply.github.com>
2020-12-23 20:39:24 +0400

    it doesn't work as it should

--- js_calc.js
+++ js_calc.js
@@ -1,27 +1,44 @@
-var op;
+let op;
 
 function calc() {
-    var result;
-    var num1 = Number(document.getElementById('num1').value);
-    var num2 = Number(document.getElementById('num2').value);
+    let prom = 0//промежуточное значение
+    let result;
+    let num1 = Number(document.getElementById('num1').value);
+    let num2 = Number(document.getElementById('num2').value);
+
     switch (op) {
+        case (num1 && num2 === undefined):
+            result = 'введите числа';
+            break;
         case '+':
             result = num1 + num2;
+            prom = result + prom;
+            document.getElementById('prom').innerHTML = prom;
             break;
         case '-':
             result = num1 - num2;
+            prom = result - prom;
+            document.getElementById('prom').innerHTML = prom;
             break;
         case '*':
             result = num1 * num2;
+            prom = result * prom;
+            document.getElementById('prom').innerHTML = prom;
             break;
         case '/':
             if (num2 !== 0) {
                 result = num1 / num2
+                prom = result / prom;
+                document.getElementById('prom').innerHTML = prom;
             } else {
                 result = 'нет ответа';
             }
             break;
-        default: result = ' выберите действие';
-            }
-document.getElementById('result').innerHTML = result;
+        default:
+            result = ' выберите действие';
+            break;
+
+    }
+
+    document.getElementById('result').innerHTML = result;
 }
