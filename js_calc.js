8cb6d9dc3fac1df27e97c633534ae881216d4c43
Олег <73531304+olegus413@users.noreply.github.com>
2021-01-27 19:39:56 +0400

    calculator_2.0.0

--- js_calc.js
+++ js_calc.js
@@ -5,84 +5,88 @@ window.onload = function () {
     let umnojit = document.getElementById('umnojit');
     let equal = document.getElementById('equal');
     let clear = document.getElementById('clear');
-    let result = 0;
-    let oper;
-    let num = [];
-
+    let arr = [];
+    let result;
+    let operation = '';
+    let num1 = 0;
+    let num = 0;
+    let history = [];
 
     function vvod() {
-        num.push(Number(document.getElementById('num').value));
-        document.getElementById('num').value = '';
-
+        arr.push(Number(document.getElementById('arr').value));
+        document.getElementById('arr').value = '';
     }
-
 
     function vivod() {
         document.getElementById('result').innerHTML = result;
-        num.join('');
-        document.getElementById('history').innerHTML = num;
-
+        history = arr.join('');
+        document.getElementById('history').innerHTML = histor;
     }
 
     clear.addEventListener('click', function (e) {
+        arr = [];
+        num1 = 0;
         num = 0;
-        document.getElementById('num').value = '';
-        document.getElementById('prom').innerHTML = '';
+        result = 0;
         document.getElementById('result').innerHTML = '';
         document.getElementById('history').innerHTML = '';
     })
 
 
     plus.addEventListener("click", function (e) {
-        oper = '+';
+        operation = '+';
         vvod();
-        num.push(oper);
+        arr.push(operation);
+        operation = '';
     })
 
     minus.addEventListener("click", function (e) {
-        oper = '-';
+        operation = '-';
         vvod();
-        num.push(oper);
+        arr.push(operation);
+        operation = '';
     })
 
     umnojit.addEventListener("click", function (e) {
-        oper = '*';
+        operation = '*';
         vvod();
-        num.push(oper);
+        arr.push(operation);
+        operation = '';
     })
 
     delit.addEventListener("click", function () {
-        if (num[num.length - 3] === 0 || num[num.length - 1] === 0) {
-            document.getElementById('vivod').innerHTML = 'Введите корректные данные';
-        } else {
-            oper = '/';
-            vvod();
-            num.push(oper);
-        }
+        operation = '/';
+        vvod();
+        arr.push(operation);
+        operation = '';
     })
 
 
     equal.addEventListener('click', function (e) {
-        if (oper === '+') {
-            vvod();
-            result = num[num.length - 3] + num[num.length - 1];
-            oper = '';
-        } else if (oper === '-') {
-            vvod();
-            result = num[num.length - 3] - num[num.length - 1];
-            oper = '';
-        } else if (oper === '*') {
-            vvod();
-            result = num[num.length - 3] * num[num.length - 1];
-            oper = '';
-        } else if (oper === '/') {
-            vvod();
-            result = num[num.length - 3] / num[num.length - 1];
-            oper = '';
+        vvod();
+        for (let i = 0; i < arr.length; i++) {
+
+            if (typeof (arr[i]) === 'string') {
+
+                operation = arr[i];
+            } else {
+
+                num1 = num;
+                num = arr[i];
+                
+                if (operation === '+') {
+                    num = num + num1;
+                } else if (operation === '-') {
+                    num = num1 - num;
+                } else if (operation === '*') {
+                    num = num * num1;
+                } else if (operation === '/') {
+                    num = num1 / num;
+                }
+
+            }
+            result = num;
         }
         vivod();
-
     })
-
-
 }
