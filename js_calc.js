1a6362a4240849c36da0d61fd079bfe3bc3292d2
Олег <73531304+olegus413@users.noreply.github.com>
2021-01-29 22:07:08 +0400

    2.1

--- js_calc.js
+++ js_calc.js
@@ -12,19 +12,58 @@ window.onload = function () {
     let num = 0;
     let history = [];
 
+
+
     function vvod() {
-        arr.push(Number(document.getElementById('arr').value));
-        document.getElementById('arr').value = '';
+        let x = document.getElementById('arr').value;
+        if (x === null || x === "") {
+            alert(" вы здесь если не ввели число \n                     или \n потому что хотите поменять знак действия");
+            return false;
+        } else {
+            arr.push(Number(document.getElementById('arr').value));
+            document.getElementById('arr').value = '';
+        }
     }
+
 
     function vivod() {
         document.getElementById('result').innerHTML = result;
         history = arr.join('');
-        document.getElementById('history').innerHTML = histor;
+        document.getElementById('history').innerHTML = history;
     }
+
+
+    function forHist() {
+        history = arr.join('');
+        document.getElementById('history').innerHTML = history;
+
+        /*if (history[history.length - 1] !== 'string') {
+            history.push(operation);
+
+        } else {
+            history.pop();
+        }
+        history = history.join('');
+        document.getElementById('history').innerHTML = history;*/
+    }
+
+
+    function forDeistv() {
+        vvod();
+
+        if (arr[arr.length - 1] !== 'string') {
+            arr.push(operation);
+            operation = '';
+        } else {
+            arr.pop();
+        }
+        forHist();
+    }
+
 
     clear.addEventListener('click', function (e) {
         arr = [];
+        history =[];
         num1 = 0;
         num = 0;
         result = 0;
@@ -35,30 +74,22 @@ window.onload = function () {
 
     plus.addEventListener("click", function (e) {
         operation = '+';
-        vvod();
-        arr.push(operation);
-        operation = '';
+        forDeistv();
     })
 
     minus.addEventListener("click", function (e) {
         operation = '-';
-        vvod();
-        arr.push(operation);
-        operation = '';
+        forDeistv();
     })
 
     umnojit.addEventListener("click", function (e) {
         operation = '*';
-        vvod();
-        arr.push(operation);
-        operation = '';
+        forDeistv();
     })
 
     delit.addEventListener("click", function () {
         operation = '/';
-        vvod();
-        arr.push(operation);
-        operation = '';
+        forDeistv();
     })
 
 
@@ -73,7 +104,7 @@ window.onload = function () {
 
                 num1 = num;
                 num = arr[i];
-                
+
                 if (operation === '+') {
                     num = num + num1;
                 } else if (operation === '-') {
@@ -89,4 +120,5 @@ window.onload = function () {
         }
         vivod();
     })
+
 }
